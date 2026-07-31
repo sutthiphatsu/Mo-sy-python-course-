293b3d1344aa7ae21e3fb006213007dedc84376e
mo'sy <sutthiphat.su@ku.th>
2026-07-31 07:37:35 +0100

    Initial commit

--- labs/week04-lab/example/list-methods.py
+++ labs/week04-lab/example/list-methods.py
@@ -32,3 +32,6 @@ print(f"Reversed fruits: {fruits}")        # ['orange', 'apple', 'banana', 'appl
 print(f"Min: {min(numbers)}")              # 1
 print(f"Max: {max(numbers)}")              # 9
 print(f"Sum: {sum(numbers)}")              # 36
+
+average = sum(numbers) / len(numbers)
+print(f"average:{average}")
